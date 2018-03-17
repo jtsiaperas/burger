665f0a5a9a332133b9712e2e33e6dac08ae45ff0
Austin Tsiaperas <liketexas@gmail.com>
2018-03-17 02:40:39 -0500

    updated package.json

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,9 @@
-drop database if exists burgers_db;
-
-create database burgers_db;
-
-use burgers_db;
-
+use i0sh31pnm26y4vhe;
+drop table burgers;
 create table burgers (
-   id int auto_increment unique,
+   id int auto_increment not null,
    burger_name varchar(140) not null,
    devoured boolean default false,
+   createdAt TIMESTAMP default current_timestamp,
    primary key (id)
 );
